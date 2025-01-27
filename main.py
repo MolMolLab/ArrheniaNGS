2219a0c5e22a541cd12830f2ad2dadfa0dcfaa9e
MolMolLab <sibmyco@gmail.com>
2025-01-28 02:05:50 +0500

    add english

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from pipeline_processor import PipelineProcessor
 
 if __name__ == "__main__":
-    folder_path = "/..." # Рабочая директория
-    sequences_folder_path = "/..." # Директория с сиквенсами
-    resources_folder_path = "/..." # Директория с классификатором, референсной базой и списком адаптеров
-    adapter_file='.json' # Список адаптеров в формате json
-    classifier_file='...qza' # Классификатор
-    reference_sequences_file="...fasta" # Референсная база
-    threads = 8 # Количество потоков
+    folder_path = "/..."                # Рабочая директория                                                  | Working directory
+    sequences_folder_path = "/..."      # Директория с сиквенсами                                             | Sequence directory
+    resources_folder_path = "/..."      # Директория с классификатором, референсной базой и списком адаптеров | Directory with classifier, reference base and list of adapters
+    adapter_file='...json'              # Список адаптеров в формате json                                     | List of adapters in json format
+    classifier_file='...qza'            # Артефакт классификатора                                             | Classifier artifact
+    reference_sequences_file="...fasta" # Референсная база в формате FASTA                                    | Reference base in FASTA format
+    threads = 8                         # Количество потоков                                                  | Number of threads
 
     processor = PipelineProcessor(folder_path, sequences_folder_path, resources_folder_path, adapter_file, classifier_file, reference_sequences_file, threads)
     processor.process_paired_files()
